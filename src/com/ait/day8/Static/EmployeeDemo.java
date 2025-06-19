40c6168135e36e08d4d7b194f045e2d9b5a6a9bf
akshathav25 <160473979+akshathav25@users.noreply.github.com>
2025-06-19 13:46:21 +0530

    Update EmployeeDemo.java

--- src/com/ait/day8/Static/EmployeeDemo.java
+++ src/com/ait/day8/Static/EmployeeDemo.java
@@ -3,7 +3,7 @@ package com.ait.day8.Static;
 public class EmployeeDemo {
 
 	public static void main(String[] args) {
-		Employee e= new Employee("Akshatha",09);
+		Employee e= new Employee("Akshatha",20);
 		System.out.println(e);
 		
 
